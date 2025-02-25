5ca55f0b79bd0ff1aec9878a684521b6d8f55e28
mauricio <mauriciochasi@gmail.com>
2025-02-26 00:42:25 +0100

    config

---
+++ microservice-clientes/src/main/java/com/mauricio/microservice/app/clientes/Config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.mauricio.microservice.app.clientes.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://market-alpha-two.vercel.app") // Reemplaza con tu URL en Vercel
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
